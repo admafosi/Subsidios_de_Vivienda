3af28001de7b771d4939a3f7613903ccd551d733
Admafosi <amforiguas@uqvirtual.edu.co>
2025-06-01 17:11:54 -0500

    Add files via upload

---
+++ Scripts/DL_Deptos_create.sql
@@ -0,0 +1,7 @@
+-- DataLakeSubViv.dbo.DL_Deptos definition
+-- DROP TABLE DataLakeSubViv.dbo.DL_Deptos;
+
+CREATE TABLE DataLakeSubViv.dbo.DL_Deptos (
+	CodDepto int NULL,
+	NomDepto nvarchar(60)
+);
