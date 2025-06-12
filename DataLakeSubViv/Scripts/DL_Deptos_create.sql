d46dcc1b2466fbbabca7825ce351ce9c9fda534e
Admafosi <amforiguas@uqvirtual.edu.co>
2025-06-12 12:19:23 -0500

    Delete DataLakeSubViv directory

--- DataLakeSubViv/Scripts/DL_Deptos_create.sql
+++
@@ -1,7 +0,0 @@
--- DataLakeSubViv.dbo.DL_Deptos definition
--- DROP TABLE DataLakeSubViv.dbo.DL_Deptos;
-
-CREATE TABLE DataLakeSubViv.dbo.DL_Deptos (
-	CodDepto int NULL,
-	NomDepto nvarchar(60)
-);
